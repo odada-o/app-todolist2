17f702b34f56ae35b1144825ee0961f5898ac7e7
odd:odd <eehd80yul@gmail.com>
2024-11-21 17:47:03 +0900

    submit 추가

--- src/components/TodoEditor.jsx
+++ src/components/TodoEditor.jsx
@@ -13,13 +13,16 @@ const TodoEditor = ({addTodo}) => {
     <div>
         <h2>새로운 Todo 작성하기</h2>
         <div>
-          <input type="text" value={task} onChange={onChangeTask} placeholder="할 일을 입력하세요." className='p-3 text-black' />
-          <button 
-            onClick={onSubmit} 
-            disabled={!task} 
-            className={
-            classNames('p-3', task ? 'bg-blue-300' : 'bg-gray-300')
-            }>할 일 추가</button>
+          <form>
+            <input type="text" value={task} onChange={onChangeTask} placeholder="할 일을 입력하세요." className='p-3 text-black' />
+            <button 
+              type='submit'
+              onClick={onSubmit} 
+              disabled={!task} 
+              className={
+              classNames('p-3', task ? 'bg-blue-300' : 'bg-gray-300')
+              }>할 일 추가</button>
+          </form>
         </div>
       </div>
   )
