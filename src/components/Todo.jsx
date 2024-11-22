8b0d7b622803cd7302d16f9ad56175706fb99c25
odd:odd <eehd80yul@gmail.com>
2024-11-22 14:55:10 +0900

    로컬스트리지 셋팅

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,6 +9,20 @@ import { mockTodoData } from '@/data/todoData'
 const Todo = () => {
   const [todos, setTodos] = useState(mockTodoData)
 
+  // 마운트 시 
+  useEffect(() => {
+    // 로컬스토리지에서 데이터를 가져와서 
+    const savedTodos = JSON.parse(localStorage.getItem('todos') || [])
+    // todos 상태에 저장
+    setTodos(savedTodos)
+  }, [])
+
+  // todos 업데이트 시
+  useEffect(() => {
+    // 로컬스토리지에 데이터를 저장
+  }, [todos])
+  
+
   const addTodo = (task) => {
     const newTodo = {
       id: todos.length + 1,
