f58487885439b7498f31330e35631eda777cf77f
odd:odd <eehd80yul@gmail.com>
2024-11-26 15:05:22 +0900

    dd

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,9 +14,27 @@ const TodoList = () => {
     )
   }
 
+
+
+  // 이 함수가 의존하는 todos 상태가 변경될 때마다 lookBack 함수가 다시 실행되고 있음
+  //   React의 Strict Mode로 인한 3배수 효과
+  // React 18의 Strict Mode는 개발 환경에서 컴포넌트를 두 번 렌더링합니다.
+  // 따라서 위의 3번 호출이 개발 환경에서 3번 발생하게 됩니다.
+  // 그래서 최종적으로: 3(JSX에서의 호출) × 3(Strict Mode로 인한 반복) = 9번 호출
+  const lookBack = () => {
+    console.log('lookback')
+    const total = todos.length;
+    const done = todos.filter((todo) => todo.isDone).length
+    const left = total - done
+    return {total, done, left};
+  }
+
   return (
     <div>
         <h2>할 일 목록</h2>
+        <div>
+            {lookBack().total}개 중에 {lookBack().done}개 완료, {lookBack().left}개 남음
+        </div>
         <input 
           type="search"
           value={search}
